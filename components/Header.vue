ec15fb7b2708cb0bb77b2af7575c410df045da6d
Breno Costa Zukowski Marques <breno.zukowski@m2center.com.br>
2020-11-28 08:25:21 -0300

    First commit.

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-black">
+    <div class="container flex justify-between">
+      <img class="logo" src="@/assets/img/book-on.png" alt="Book On" />
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+<style lang="scss">
+.logo {
+  height: 135px;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+.container{
+  max-width: 90%;
+  @apply mx-auto;
+}
+</style>
